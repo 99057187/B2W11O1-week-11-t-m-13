1bd3e126fe8f6ffa06ea7e0db9a679beac9dcbcf
anthony <99057187@mydavinci.nl>
2019-11-21 22:20:56 +0100

    no message

--- week 11 T&M 13/B2W11O1 arrays.js
+++ week 11 T&M 13/B2W11O1 arrays.js
@@ -1,41 +1,35 @@
-var weekdagen = ["maandag <br>","dinsdag <br>","woensdag <br>","donderdag <br>","vrijdag <br>","zaterdag <br>","zondag <br>"]
-document.write("Alle dagen van de week zijn: <br>");
+var weekdagen = ["maandag ","dinsdag ","woensdag ","donderdag ","vrijdag ","zaterdag ","zondag "];
 var listje = document.getElementById("list");
-listje.innerHTML = weekdagen[0];
-document.write(weekdagen[1]);
-document.write(weekdagen[2]);
-document.write(weekdagen[3]);
-document.write(weekdagen[4]);
-document.write(weekdagen[5]);
-document.write(weekdagen[6]);
-document.write("<br>")
-document.write("De werkdagen zijn: <br>");
-document.write(weekdagen[0]);
-document.write(weekdagen[1]);
-document.write(weekdagen[2]);
-document.write(weekdagen[3]);
-document.write(weekdagen[4]);
-document.write("<br>")
-document.write("De weekenddagen zijn: <br>");
-document.write(weekdagen[5]);
-document.write(weekdagen[6]);
-document.write("<br>")
-document.write("Alle dagen van de week in omgekeerde volgorde zijn: <br>");
-document.write(weekdagen[6]);
-document.write(weekdagen[5]);
-document.write(weekdagen[4]);
-document.write(weekdagen[3]);
-document.write(weekdagen[2]);
-document.write(weekdagen[1]);
-document.write(weekdagen[0]);
-document.write("<br>")
-document.write("De werkdagen in omgekeerde volgorde zijn: <br>");
-document.write(weekdagen[4]);
-document.write(weekdagen[3]);
-document.write(weekdagen[2]);
-document.write(weekdagen[1]);
-document.write(weekdagen[0]);
-document.write("<br>")
-document.write("De weekenddagen in omgekeerde volgorde zijn: <br>");
-document.write(weekdagen[6]);
-document.write(weekdagen[5]);
+var tekst  = document.getElementById("tekst");
+tekst.innerHTML = "De week dagen zijn: "
+listje.innerHTML = weekdagen;
+
+var werkdagen = ["maandag ","dinsdag ","woensdag ","donderdag ","vrijdag "];
+var listje = document.getElementById("list2");
+var tekst  = document.getElementById("tekst2");
+tekst.innerHTML = "De werkdagen zijn: ";
+listje.innerHTML = werkdagen;
+
+var weekend = ["zaterdag ","zondag "];
+var listje = document.getElementById("list3");
+var tekst  = document.getElementById("tekst3");
+tekst.innerHTML = "De weekenddagen zijn: ";
+listje.innerHTML = weekend;
+
+var onweekdagen = ["zondag ","zaterdag ","vrijdag ","donderdag ","woensdag ","dinsdag ","maandag "];
+var listje = document.getElementById("list4");
+var tekst  = document.getElementById("tekst4");
+tekst.innerHTML = "De week dagen in omgekeerde volgorde zijn: ";
+listje.innerHTML = onweekdagen;
+
+var onwerkdagen = ["vrijdag ","donderdag ","woensdag ","dinsdag ","maandag "];
+var listje = document.getElementById("list5");
+var tekst  = document.getElementById("tekst5");
+tekst.innerHTML = "De werkdagen in omgekeerde volgorde zijn: ";
+listje.innerHTML = onwerkdagen;
+
+var onweekenddagen = ["zondag ","zaterdag "];
+var listje = document.getElementById("list6");
+var tekst  = document.getElementById("tekst6");
+tekst.innerHTML = "De weekenddagen in omgekeerde volgorde zijn: ";
+listje.innerHTML = onweekenddagen;
--- week 11 T&M 13/B2W11O2 arrays2.js
+++ week 11 T&M 13/B2W11O2 arrays2.js
@@ -1,2 +1,20 @@
-var namen = [prompt("Hoeveel namen wilt u in de array stoppen? (minimaal 3)")];
-document.write.h2(namen[0]);
+var Hoeveel = [prompt("Hoeveel namen wilt u in de array stoppen? (minimaal 3)")];
+if(Hoeveel < 3){
+	alert("u dient een getal hoger of gelijk aan 3 te kiezen");
+}
+else if(Hoeveel >= 3){
+	var namen = prompt("welke namen wilt u invoegen");
+	var array = namen.split(",");
+	for (var i = 0; i < Hoeveel; i++){
+		document.getElementById("naam " + i).innerHTML = array[+ i];
+	}
+	//omgekeerd
+	i--
+	for (var j = 0; j < Hoeveel; j++){
+		document.getElementById("onnaam " + i).innerHTML = array[+ j];
+		i--
+	}
+}
+else{
+	alert("u dient een getal in te typen");
+}
