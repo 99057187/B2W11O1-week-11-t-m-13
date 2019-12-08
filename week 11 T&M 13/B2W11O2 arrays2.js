8ee2a773c4d04c3f1d8ad41cde6d12534347c68d
anthony <99057187@mydavinci.nl>
2019-12-08 22:06:16 +0100

    no message

--- week 11 T&M 13/B2W11O2 arrays2.js
+++ week 11 T&M 13/B2W11O2 arrays2.js
@@ -1,18 +1,21 @@
-var Hoeveel = [prompt("Hoeveel namen wilt u in de array stoppen? (minimaal 3)")];
-if(Hoeveel < 3){
+var hoeveel = prompt("Hoeveel namen wilt u in de array stoppen? (minimaal 3)");
+if(hoeveel < 3){
 	alert("u dient een getal hoger of gelijk aan 3 te kiezen");
 }
-else if(Hoeveel >= 3){
-	var namen = prompt("welke namen wilt u invoegen");
+else if(hoeveel >= 3){
+	var namen = prompt("welke namen wilt u invoegen(met comma's zonder spaties)");
 	var array = namen.split(",");
-	for (var i = 0; i < Hoeveel; i++){
-		document.getElementById("naam " + i).innerHTML = array[+ i];
+	for (var i = 0; i < hoeveel; i++){
+		var kopje = document.createElement("h4");
+		kopje.innerHTML = array[i];
+		document.body.appendChild(kopje);
 	}
 	//omgekeerd
-	i--
-	for (var j = 0; j < Hoeveel; j++){
-		document.getElementById("onnaam " + i).innerHTML = array[+ j];
-		i--
+	for (var j = 0; j < hoeveel; j++){
+		array.reverse();
+		var kopje = document.createElement("h4");
+		kopje.innerHTML = array[j];
+		document.body.appendChild(kopje);
 	}
 }
 else{
